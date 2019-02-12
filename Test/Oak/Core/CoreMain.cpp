5f005df27d4bad7116e088659b6dde614496893e
suudai <34059792+n-suudai@users.noreply.github.com>
2019-02-13 00:40:39 +0900

    CoreMain.cpp追加

---
+++ Test/Oak/Core/CoreMain.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "Oak/Platform/OS/Win32.hpp"
+
+
+#define ExecuteTest(testName) testName ## TestMain()
+
+
+
+int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+
+    return 0;
+}
+
